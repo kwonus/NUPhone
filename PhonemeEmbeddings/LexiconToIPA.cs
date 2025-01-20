1f4c3827a7edb56433ea3784e795536372e52f87
Kevin Wonus <kevin@wonus.com>
2025-01-20 00:58:20 -0500

    More sophisticated locater for enus.txt file

--- PhonemeEmbeddings/LexiconToIPA.cs
+++ PhonemeEmbeddings/LexiconToIPA.cs
@@ -25,9 +25,10 @@ namespace PhonemeEmbeddings
 
             this.ipa_primatives = new();
 
-            string path = Path.Combine((home != null ? home : "C:\\src\\NUPhone\\PhonemeEmbeddings"), "en_US.txt").Replace('\\', '/');
-			try
-			{
+            string path = ENUS.Replace('\\', '/');
+
+            try
+            {
 				if (File.Exists(path))
 				{
 					using (StreamReader file = new StreamReader(path))
@@ -68,6 +69,119 @@ namespace PhonemeEmbeddings
 				System.Console.WriteLine("IO Error encountered");
                 this.ipa_primatives.Clear();
             }
-		}		
-	}
+		}
+        private static string _enus = null;
+        private static string ENUS
+        {
+            get
+            {
+                if (_enus != null)
+                    return _enus;
+
+                string cwd = Directory.GetCurrentDirectory();
+                for (string en = Path.Combine(cwd, "Digital-AV", "en_us.txt"); en.Length > @"X:\Digital-AV\en_us.txt".Length; en = Path.Combine(cwd, "Digital-AV", "en_us.txt"))
+                {
+                    if (System.IO.File.Exists(en))
+                    {
+                        _enus = en;
+                        return en;
+                    }
+                    var parent = Directory.GetParent(cwd);
+                    if (parent == null)
+                        break;
+                    cwd = parent.FullName;
+                }
+                cwd = Directory.GetCurrentDirectory();
+                for (string en = Path.Combine(cwd, "AV-Bible", "Digital-AV", "en_us.txt"); en.Length > @"X:\Digital-AV\en_us.txt".Length; en = Path.Combine(cwd, "AV-Bible", "Digital-AV", "en_us.txt"))
+                {
+                    if (System.IO.File.Exists(en))
+                    {
+                        _enus = en;
+                        return en;
+                    }
+                    var parent = Directory.GetParent(cwd);
+                    if (parent == null)
+                        break;
+                    cwd = parent.FullName;
+                }
+                cwd = Directory.GetCurrentDirectory();
+                for (string en = Path.Combine(cwd, "AVBible", "Digital-AV", "en_us.txt"); en.Length > @"X:\Digital-AV\en_us.txt".Length; en = Path.Combine(cwd, "AVBible", "Digital-AV", "en_us.txt"))
+                {
+                    if (System.IO.File.Exists(en))
+                    {
+                        _enus = en;
+                        return en;
+                    }
+                    var parent = Directory.GetParent(cwd);
+                    if (parent == null)
+                        break;
+                    cwd = parent.FullName;
+                }
+                cwd = Directory.GetCurrentDirectory();
+                for (string en = Path.Combine(cwd, "en_us.txt"); en.Length > @"X:\en_us.txt".Length; en = Path.Combine(cwd, "en_us.txt"))
+                {
+                    if (System.IO.File.Exists(en))
+                    {
+                        _enus = en;
+                        return en;
+                    }
+                    var parent = Directory.GetParent(cwd);
+                    if (parent == null)
+                        break;
+                    cwd = parent.FullName;
+                }
+                _enus = GetProgramDirDefault("Digital-AV", "en_us.txt");
+                return _enus;
+            }
+        }
+        private static string GetProgramDirDefault(string collection, string file)
+        {
+            var folders = new string[] { "AV-Bible", "Digital-AV", "DigitalAV" };
+
+            foreach (string folder in folders)
+            {
+                string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                string candidate = Path.Combine(appdata, "Local", "Programs", folder, collection, file);
+                if (System.IO.File.Exists(candidate))
+                {
+                    return candidate;
+                }
+            }
+
+            var roots = new string[0];
+            try
+            {
+                roots = new string[] { Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()), @"C:\", @"D:\", @"E:\", @"F:\" };
+            }
+            catch
+            {
+                roots = new string[] { @"C:\", @"D:\", @"E:\", @"F:\" };
+            }
+
+            foreach (string root in roots)
+            {
+                foreach (string folder in folders)
+                {
+                    string candidate = Path.Combine(root, "Program Files", folder, collection, file);
+                    if (System.IO.File.Exists(candidate))
+                    {
+                        return candidate;
+                    }
+                }
+            }
+
+            foreach (string root in roots)
+            {
+                foreach (string folder in folders)
+                {
+                    string candidate = Path.Combine(root, "Program Files (x86)", folder, collection, file);
+                    if (System.IO.File.Exists(candidate))
+                    {
+                        return candidate;
+                    }
+                }
+            }
+            return String.Empty;
+        }
+    }
 }
